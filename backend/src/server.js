048062ea93ca212c1ab6974c45a1b165d8998922
Asish Jacob <asishjac@gmail.com>
2025-11-07 18:46:02 +0100

    Update frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 const app = express();
+
+if(process.env.node_env === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+}
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
