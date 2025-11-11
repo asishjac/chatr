77c6c2d95a3dfa649fec7d03f7f136c6cc07cca2
Asish Jacob <asishjac@gmail.com>
2025-11-11 18:25:35 +0100

    implement signup controller

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (usetId,res) => {
+    const token = jwt.sign({usetId}, process.env.JWT_SECRET, {
+        expiresIn: '7d',
+    });
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,20 +3,27 @@ import dotenv from "dotenv";
 import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import {connectDB} from "./lib/db.js";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
 const app = express();
-
-if(process.env.node_env === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/build")));
-}
+app.use(express.json()); // use middleware to parse JSON request bodies
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (_ , res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    });
+}
+
 app.listen(port, () => {
   console.log('Server is running on port : ' + port);
+  connectDB();
 });
