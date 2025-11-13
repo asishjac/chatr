af85381f1b6afd242399cd19e7e8bf7615f12ce0
Asish Jacob <asishjac@gmail.com>
2025-11-13 11:36:16 +0100

    implement rate limiting middleware with arcjet

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {signup,login,logout,updateProfile} from "../controllers/auth.controller.js";
 import {protectRoute} from "../middlewares/auth.middleware.js";
+import {arcjetProtection} from "../middlewares/arcjet.middleware.js";
 
 const router = express.Router();
 
+router.use(arcjetProtection);
 router.post("/signup",signup);
 router.post("/login",login);
 router.post("/logout",logout);
